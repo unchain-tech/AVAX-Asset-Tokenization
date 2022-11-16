97021e5b71df69d278263e21aaa6c8e98c75d0a4
ryojiroakiyama <ryojiro25@gmail.com>
2022-11-16 18:13:50 +0900

    contract base

---
+++ contract/contracts/Web3Mint.sol
@@ -0,0 +1,79 @@
+// SPDX-License-Identifier: UNLICENSED
+pragma solidity ^0.8.17;
+import "@openzeppelin/contracts/token/ERC721/ERC721.sol";
+
+import "@openzeppelin/contracts/utils/Counters.sol";
+
+import "./libraries/Base64.sol";
+import "hardhat/console.sol";
+
+contract Web3Mint is ERC721 {
+    string public farmerName;
+    string public cropsName;
+    string public description;
+    uint256 public totalMint;
+    uint256 public availableMint;
+    uint256 public price;
+    uint256 public expirationDate;
+
+    using Counters for Counters.Counter;
+
+    Counters.Counter private _tokenIds;
+
+    constructor(
+        string memory _farmerName,
+        string memory _cropsName,
+        string memory _description,
+        uint256 _totalMint,
+        uint256 _availableMint,
+        uint256 _price,
+        uint256 _expirationDate
+    ) ERC721("Crops NFT", "crop") {
+        farmerName = _farmerName;
+        cropsName = _cropsName;
+        description = _description;
+        totalMint = _totalMint;
+        availableMint = _availableMint;
+        price = _price;
+        expirationDate = _expirationDate;
+        console.log("This is my NFT contract.");
+    }
+
+    function mintNFT() public {
+        uint256 newItemId = _tokenIds.current();
+        _safeMint(msg.sender, newItemId);
+        console.log(
+            "An NFT w/ ID %s has been minted to %s",
+            newItemId,
+            msg.sender
+        );
+        _tokenIds.increment();
+    }
+
+    function tokenURI(uint256 _tokenId)
+        public
+        view
+        override
+        returns (string memory)
+    {
+        string memory json = Base64.encode(
+            bytes(
+                string(
+                    abi.encodePacked(
+                        '{"name": "',
+                        cropsName,
+                        " -- NFT #: ",
+                        Strings.toString(_tokenId),
+                        '", "description": "An epic NFT", "image": "ipfs://',
+                        "", //TODO: 画像も入れるか
+                        '"}' //TODO: この他の属性を追加するか
+                    )
+                )
+            )
+        );
+        string memory output = string(
+            abi.encodePacked("data:application/json;base64,", json)
+        );
+        return output;
+    }
+}
