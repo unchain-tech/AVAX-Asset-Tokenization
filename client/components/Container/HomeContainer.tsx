b71869aa2c2c2aa304c8c8e0fc28bed230594c06
ryojiroakiyama <ryojiro25@gmail.com>
2022-11-21 02:07:14 +0900

    done home page

--- client/components/Container/HomeContainer.tsx
+++ client/components/Container/HomeContainer.tsx
@@ -4,17 +4,21 @@ import styles from "./HomeContainer.module.css";
 
 export default function HomeContainer() {
   return (
-    <div className={styles.centerContent}>
-      <ForFarmerButton
-        linkTo={"/FarmerPage"}
-        icon_left={<MdAgriculture size={50} />}
-        description={"For Farmer"}
-      />
-      <ForFarmerButton
-        linkTo={"/BuyerPage"}
-        icon_left={<MdList size={50} />}
-        description={"For Buyer"}
-      />
+    <div>
+      <div className={styles.centerContent}>
+        <ForFarmerButton
+          linkTo={"/FarmerPage"}
+          icon_left={<MdAgriculture size={50} />}
+          description={"For Farmer"}
+        />
+      </div>
+      <div className={styles.centerContent}>
+        <ForFarmerButton
+          linkTo={"/BuyerPage"}
+          icon_left={<MdList size={50} />}
+          description={"For Buyer"}
+        />
+      </div>
     </div>
   );
 }
